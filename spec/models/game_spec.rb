440102ae2f5b6726d2594c7910e55753c0bf6c91
jmontanholi <jonunes9326@hotmail.com.br>
2021-12-09 16:41:50 -0300

    Finish tests for model creation

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,85 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create(username: 'Pedro')
+    @game = @user.games.create(
+      name: 'Test game',
+      description: 'Test game description',
+      icon: 'http//test.com/image25123',
+      price: 145,
+      category: 'Horror',
+    )
+  end
+
+  context 'Add a game' do
+    it 'Marks as invalid if you do not put a name' do
+      @game.name = ''
+      expect(@game).to be_invalid
+    end
+
+    it 'Marks as invalid if the name exceeds 50 characters' do
+      @game.name = 'Something1Something1Something1Something1Something10'
+      expect(@game).to be_invalid
+    end
+
+    it 'Marks as invalid if you do not put a description' do
+      @game.description = ''
+      expect(@game).to be_invalid
+    end
+
+    it 'Marks as invalid if the description exceeds 500 characters' do
+      @game.description = '
+      Something1Something1Something1Something1Something1Something1Something1Something1Something1Something1
+      Something1Something1Something1Something1Something1Something1Something1Something1Something1Something1
+      Something1Something1Something1Something1Something1Something1Something1Something1Something1Something1
+      Something1Something1Something1Something1Something1Something1Something1Something1Something1Something1
+      Something1Something1Something1Something1Something1Something1Something1Something1Something1Something10
+      '
+      expect(@game).to be_invalid
+    end
+
+    it 'Marks as invalid if you do not put a icon' do
+      @game.icon = ''
+      expect(@game).to be_invalid
+    end
+
+    it 'Marks as invalid if the icon exceeds 255 characters' do
+      @game.icon = '
+      Something1Something1Something1Something1Something1Something1Something1Something1Something1Something1
+      Something1Something1Something1Something1Something1Something1Something1Something1Something1Something1
+      Something1Something1Something1Something1Something1Something1Something1Something1Something1Something1
+      '
+      expect(@game).to be_invalid
+    end
+
+    it 'Marks as invalid if you do not put a price' do
+      @game.price = nil
+      expect(@game).to be_invalid
+    end
+
+    it 'Marks as invalid if the price is less or equal to 0' do
+      @game.price = -1
+      expect(@game).to be_invalid
+    end
+
+    it 'Marks as invalid if the price is not a number' do
+      @game.price = 'a'
+      expect(@game).to be_invalid
+    end
+
+    it 'Marks as invalid if you do not put a category' do
+      @game.category = ''
+      expect(@game).to be_invalid
+    end
+
+    it 'Marks as invalid if the category exceeds 50 characters' do
+      @game.category = 'Something1Something1Something1Something1Something10'
+      expect(@game).to be_invalid
+    end
+
+    it 'Marks as valid if everything else passes' do
+      expect(@game).to be_valid
+    end
+  end
 end
