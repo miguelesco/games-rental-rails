1a0106db57ba34b06bc8fc4618983e2ab2868faa
jmontanholi <jonunes9326@hotmail.com.br>
2021-12-13 13:45:44 -0300

    Change login and sign up responses to match

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,8 +4,13 @@ class Api::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      @games = @user.games
+      @reservations = @user.reservations
       render json: {
-        user: @user
+        id: @user.id,
+        username: @user.username,
+        games: @games,
+        reservations: @reservations
       }, status: 200
     else
       render json: {
