82546073c0718e348738bc26afcb1dccf29660ab
jmontanholi <jonunes9326@hotmail.com.br>
2021-12-14 12:58:54 -0300

    heroku first

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Be sure to restart your server when you modify this file.
 
 # Avoid CORS issues when API is called from the frontend app.
@@ -15,21 +14,3 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get, :post, :delete, :put]
   end
 end
-=======
-# Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
->>>>>>> 3415fd9525001a732291e88039755322f9fc8cd0
