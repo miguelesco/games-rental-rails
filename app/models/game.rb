514d36ed3009815964cd99619f28f25ff9ed8465
jmontanholi <jonunes9326@hotmail.com.br>
2021-12-20 11:25:32 -0300

    Change reservation dependency to be destroyed

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
   belongs_to :owner, class_name: 'User'
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 500 }
